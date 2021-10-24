3f735b3f4407dd0f8f20e16f6932c819d07bd1a5
Lavish Jain <lavishjain36@gmail.com>
2021-10-24 18:13:16 +0530

    dom events

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
-function addNew() {
-  //select the target element that list
-  let select = document.getElementById("list");
+//select the target element
 
-  //create a new option
-  let newOption = document.createElement("option");
+let selectBox = document.querySelector("#select-tag");
 
-  //select the input country using textbox
-  newOption.text = document.getElementById("textbox").value;
-  select.add(newOption);
-}
+//lets add event listeners
+selectBox.addEventListener("change", function () {
+  //i will select a value
+  let selectedOption = selectBox.value;
+
+  let spanTag = document.querySelector("#span-1");
+  console.log(spanTag);
+
+  spanTag.innerText = selectedOption;
+  console.log(selectBox);
+});
+
+//we are wrting the logic to reverse a string
+let reverseString = (str) => {
+  //creating a empty string
+  let tempstr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    tempstr += str[i];
+  }
+  return tempstr;
+};
+
+//keyup event
+let textBox = document.querySelector("#username");
+textBox.addEventListener("keyup", function () {
+  let textEnter = textBox.value;
+  console.log(textEnter);
+  let usertext = document.querySelector("#user-text");
+  usertext.innerText = reverseString(textEnter);
+});
+
+let textArea = document.querySelector("#text-area");
+let count = 100;
+textArea.addEventListener("keyup", function () {
+  let textEnteredLength = textArea.value.length;
+  console.log(textEnteredLength);
+  let spanTag = document.querySelector("#span-2");
+  spanTag.innerText = count - textEnteredLength;
+  if (spanTag.innerText == 0) {
+    alert("Cant enter more data");
+  }
+});
+
+// index=0
