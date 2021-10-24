d50a7fa021b8b95730543e69f93c75c7c53e4b50
Lavish Jain <lavishjain36@gmail.com>
2021-10-24 19:26:20 +0530

    dom color

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+//array of colors
+
 let colors = ["red", "blue", "orange", "green", "yellow", "indigo", "violet"];
 
 let container = document.querySelector("#container");
 let h1 = document.querySelector("h1");
-let clickedColor = document.querySelector("#clickedColor");
+
+let clickedcolor = document.querySelector("#clickedcolor");
 
 for (const color of colors) {
   let box = document.createElement("div");
   box.classList.add("box");
   container.appendChild(box);
-
   box.style.backgroundColor = color;
   box.addEventListener("click", function () {
-    h1.innerHTML = "You have selected: ";
-    clickedColor.innerHTML = color;
-    clickedColor.style.backgroundColor = color;
+    h1.innerHTML = "You have selected";
+    clickedcolor.innerHTML = color;
+    clickedcolor.style.backgroundColor = color;
   });
 }
